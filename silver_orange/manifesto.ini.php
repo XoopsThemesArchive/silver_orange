f644fdc027c445fa578fb49b72ab9e5b3ca70df0
mambax7 <mambax7@gmail.com>
2015-05-04 05:49:48 -0400

    Original files

---
+++ silver_orange/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="silver_orange"
+Depends=Legacy_RenderSystem
+Url="http://www.expiece.com/html/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=hi-debow @ EXTRAFFIC
+ScreenShot="screenshot.png"
+Description="2 columns fixed width 850px tableless CSS layout style theme"
+
+License="Attribution-Noncommercial 2.1 Japan"
+
+*/
+?>
